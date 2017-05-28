6f76176fb940fd25d2d31ad41b962922c4d82efe
Haichen Shen <haichen@cs.washington.edu>
2017-05-27 20:05:45 -0700

    update

--- init.el
+++ init.el
@@ -4,14 +4,27 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/")
+             t)
 (package-initialize) ;; You might already have this line
+
+(setq package-list '(protobuf-mode ido))
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;; enable line number
 (require 'linum)
 (global-linum-mode 1)
 (setq-default column-number-mode 1)
-(setq linum-format "%d")
+(setq linum-format "%d ")
+
+;; set meta key to window
+(setq x-super-keysym 'meta)
+
+;; enable ido
+(require 'ido)
+(ido-mode t)
 
 ;; use Shift+arrow_keys to move cursor around split panes
 (windmove-default-keybindings)
@@ -22,6 +35,7 @@
 (load-file "~/.emacs.d/color-theme/themes/material-theme.el")
 
 ;; column indicator
+(setq-default fill-column 80)
 (require 'fill-column-indicator)
 (setq-default fci-rule-column 80)
 (setq-default fci-rule-width 1)
@@ -35,6 +49,7 @@
 
 (require 'cc-mode)
 (require 'google-c-style)
+(require 'protobuf-mode)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
 (add-hook 'c++-mode-common-hook 'google-set-c-style)
@@ -43,13 +58,23 @@
 (add-hook 'python-mode-hook
           (lambda()
             (setq indent-tabs-mode nil)
-            (setq tab-width 2)
-            (setq python-indent 2)))
+            (setq tab-width 4)
+            (setq python-indent 4)))
 
 (add-hook 'go-mode-hook
           (lambda ()
             (setq indent-tabs-mode t)
             (setq tab-width 4)))
+
+(add-hook 'racket-mode-hook
+          (lambda ()
+            (define-key racket-mode-map (kbd "C-c r") 'racket-run)))
+
+(add-to-list 'auto-mode-alist '("\\.h$" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cu$" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cuh$" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.rkt$" . racket-mode))
+(add-to-list 'auto-mode-alist '("\\.proto$" . protobuf-mode))
 
 ;; auto complete
 (require 'auto-complete-config)
@@ -69,15 +94,47 @@
 (setq company-tooltip-align-annotations t)
 
 
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(column-number-mode 1))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((t (:inherit nil :stipple nil :background "#263238" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(column-number-mode 1))
+ '(package-selected-packages
+   (quote
+    (racket-mode dr-racket-like-unicode cuda-mode auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#263238" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco")))))
+ )
+
+(defun sort-buffers ()
+  "Put the buffer list in alphabetical order."
+  (interactive)
+  (dolist (buff (buffer-list-sorted)) (bury-buffer buff))
+  (when (interactive-p) (list-buffers))
+  )
+
+(defun buffer-list-sorted ()
+  (sort (buffer-list) 
+        (function
+         (lambda
+           (a b) (string<
+                  (buffer-file-name a)
+                  (buffer-file-name b)
+                  )))))
+
+(global-set-key "\M-s" 'sort-buffers)
