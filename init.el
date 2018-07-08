888f5bd2f09cd9e78eb6663cbefbb6575d6f387d
Haichen Shen <shenhaichen@gmail.com>
2018-07-07 18:07:59 -0700

    x

--- init.el
+++ init.el
@@ -8,7 +8,13 @@
              t)
 (package-initialize) ;; You might already have this line
 
-(setq package-list '(protobuf-mode ido))
+(setq package-list '(protobuf-mode auto-complete ido go-mode cuda-mode))
+;; activate all the packages (in particular autoloads)
+(package-initialize)
+;; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+;; install the missing packages
 (dolist (package package-list)
   (unless (package-installed-p package)
     (package-install package)))
@@ -100,12 +106,12 @@
 ;;  ;; Your init file should contain only one such instance.
 ;;  ;; If there is more than one, they won't work right.
 ;;  '(column-number-mode 1))
-;; (custom-set-faces
+(custom-set-faces
 ;;  ;; custom-set-faces was added by Custom.
 ;;  ;; If you edit it by hand, you could mess it up, so be careful.
 ;;  ;; Your init file should contain only one such instance.
 ;;  ;; If there is more than one, they won't work right.
-;;  '(default ((t (:inherit nil :stipple nil :background "#263238" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco")))))
+  '(default ((t (:inherit nil :stipple nil :background "#263238" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Roboto Mono")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
